53090c771defdcbf85049c67b73dedd65666530f
trac <trac@ubuntu.ubuntu-domain>
2012-04-14 11:09:54 -0400

    model comments

--- application/models/Achievement.php
+++ application/models/Achievement.php
@@ -1,21 +1,35 @@
 <?php
 class Achievement extends Aware
 {
+    /**
+     * Accessible
+     *  for mass-assignment...
+     */
     public static $accessible = array('name', 'value', 'description');
 
+    /**
+     * [Many to Many] Users
+     * 
+     * @return array
+     */
     public function users()
     {
         $this -> has_many_and_belongs_to('Users');
     }
 
-    public function achievements()
+    /**
+     * [Many to Many] requirements
+     * 
+     * @return array
+     */
+    public function requirements()
     {
         $this -> has_many_and_belongs_to('Requirements');
     }
 
     /**
     *
-    * Aware validation
+    * Aware validation rules
     *
     */
     public static $rules = array (
--- application/models/Requirement.php
+++ application/models/Requirement.php
@@ -1,8 +1,18 @@
 <?php
 class Requirement extends Aware
 {
+    /**
+     * Accessible
+     *  for mass-assignment...
+     */
     public static $accessible = array('noun', 'verb', 'value');
 
+
+    /**
+     * [Many to Many] Achievements
+     * 
+     * @return array
+     */
     public function achievement()
     {
         $this -> has_many_and_belongs_to('Achievements');
@@ -10,7 +20,7 @@ class Requirement extends Aware
 
     /**
     *
-    * Aware validation
+    * Aware validation 
     *
     */
     public static $rules = array (
