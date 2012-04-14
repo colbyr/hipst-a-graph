f74458e06b794ddb7ade2e11244a8772b7022f54
trac <trac@ubuntu.ubuntu-domain>
2012-04-14 02:30:48 -0400

    Achievement model

---
+++ application/models/Achievement.php
@@ -0,0 +1,16 @@
+<?php
+class Achievement extends Aware
+{
+    public static $accessible = array('name', 'value');
+
+    public function users()
+    {
+        $this -> has_many_and_belongs_to('Users');
+    }
+
+    public function achievements()
+    {
+        $this -> has_many_and_belongs_to('Requirements');
+    }
+
+}
