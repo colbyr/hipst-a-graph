daac8b299aabb834b6726965f547e755cfc23e08
Dan Miller <dan@danielsmiller.com>
2012-04-14 02:33:33 -0400

    Added users migration.

---
+++ application/migrations/2012_04_14_062532_create_users.php
@@ -0,0 +1,29 @@
+<?php
+
+class Create_Users {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+      Schema::create('users', function($table) {
+        $table->increments('id');
+        $table->string('oauth_token', 320);
+        $table->string('oauth_token_secret', 320);
+      });
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+      Scheme::drop('users');
+	}
+
+}
