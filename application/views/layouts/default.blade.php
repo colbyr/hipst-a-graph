3e35582e0c4fa3d897aa0767351c5d2672c04f8d
Colby Rabideau <crabideau5691@gmail.com>
2012-04-15 13:51:58 -0400

    added page icons

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -10,6 +10,10 @@
 @section('styles')
 @yield_section
 
+<!-- icons -->
+<link rel="shortcut icon" href="{{ URL::to_asset('img/logo-xsmall.png') }}"/>
+<link rel="apple-touch-icon-precomposed" href="{{ URL::to_asset('img/logo-large.png') }}"/>
+
 <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
