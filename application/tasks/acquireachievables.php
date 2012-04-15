23d47aa9d9d33621fc305d570387f65b28cdb108
Dan Miller <dan@danielsmiller.com>
2012-04-15 10:17:31 -0400

    Forgot to add new file.

---
+++ application/tasks/acquireachievables.php
@@ -0,0 +1,79 @@
+<?php
+public class AquireAchievables
+{
+
+    public function getEtsyFunction($query)
+    {
+        return Etsy::$query();
+    }
+
+    public function run($user)
+    {
+        //get an array of all of the achievement already earned
+        $achievements = getAchievements($user);
+        
+        //get the achievements not earned yet
+        //array of unearned achievements
+        $unearned_achievements = unearned($achievements);
+
+        //here is where we should do it in a background process
+        //but for now lets do this:
+        //loop through the array of unearned achievements
+        
+        $json;
+        $query;
+        $oldquery;
+
+        foreach($unearned_achievements as $unearned)
+        {
+            //get query
+            $query = $unearned->query;
+
+            if($query != $oldquery){
+                $json = getEtsyFunction($query);
+            }
+
+            if(check($json, $unearned))
+                $user->achievements()->attach($unearned->id);
+
+            $oldquery = $query;
+       }
+    }
+
+    public function check($json, $unearned){
+      $noun = $unearned->noun;
+      $verb = $unearned->verb;
+      $value = $unearned->value;
+
+      //TODO: Search JSON for the noun of $unearned
+      //Compare values between $unearned and $json (with $verb)
+      //return a boolean (is above comparison true or false)
+    }
+
+    public function getAchievements($user)
+    {
+        return $user->achievements()->get();
+    }
+
+    public function unearned($achievements)
+    {
+        $array;
+        foreach ($achievements as $achievement) 
+        {
+            array_push($array, $achievement->id);
+        }
+        return $unearned = DB::table('achievements')
+                ->where_not_in('id', $array)
+                ->order_by('query', 'desc')
+                ->get();
+    }
+
+    // public function buildArray($unearned)
+    // {
+    //     foreach ($unearned as $achievement) 
+    //     {
+            
+    //     }
+    // }
+
+}
