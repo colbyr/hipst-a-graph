ebb13eb64fcc02345af885e311fe7c6cb748b36c
trac <trac@ubuntu.ubuntu-domain>
2012-04-15 00:49:01 -0400

    getting achievments

--- application/libraries/aquireachievables.php
+++ application/libraries/aquireachievables.php
@@ -10,26 +10,60 @@ public class AquireAchievables
     public function aquire($user)
     {
         //get an array of all of the achievement already earned
-        $achivements = getAchievements($user);
+        $achievements = getAchievements($user);
         
-        //get the achivements not earned yet
-        //mutlidementional [query][object]
-        $open_achivements = unearned($achivements);
+        //get the achievements not earned yet
+        //array of unearned achievements
+        $unearned_achievements = unearned($achievements);
 
         //here is where we should do it in a background process
         //but for now lets do this:
-        //loop through the array of unearned achivements
-        foreach($open_achivements as $query)
+        //loop through the array of unearned achievements
+        
+        $json;
+        $query;
+        $oldquery;
+
+        foreach($unearned_achievements as $unearned)
         {   
-            //make the query
-            $json = getEtsyFunction($query);
-            //loop through the values
-            foreach ($query as $achievement) 
-            {
-                //check the achivement
-                if(check($achievement))
-                    $user->achivements()->attach($achivement->id);
+            //get query
+            $query = $unearned->query;
+
+            if($query != $oldquery){
+                $json = getEtsyFunction($query);
             }
-        }
+
+            if(check($json, $unearned))
+                $user->achievements()->attach($unearned->id);
+
+            $oldquery = $query;
+       }
     }
+
+    public function getAchievements($user)
+    {
+        return $user->achievements()->get();
+    }
+
+    public function unearned($achievements)
+    {
+        $array;
+        foreach ($achievements as $achievement) 
+        {
+            array_push($array, $achievement->id);
+        }
+        return $unearned = DB::table('achievements')
+                ->where_not_in('id', $array)
+                ->order_by('query', 'desc')
+                ->get();
+    }
+
+    // public function buildArray($unearned)
+    // {
+    //     foreach ($unearned as $achievement) 
+    //     {
+            
+    //     }
+    // }
+
 }
