11dde9033ef974f6c1edf55114886ac6fd38d4fa
Dan Miller <dan@danielsmiller.com>
2012-04-22 23:40:31 -0400

    Had the mysql params in the wrong order.

--- application/config/database.php
+++ application/config/database.php
@@ -71,9 +71,9 @@ return array(
 			'driver'   => 'mysql',
 			'host'     => $_SERVER["DB1_HOST"],
 			'database' => $_SERVER["DB1_NAME"],
-			'username' => $_SERVER["DB1_NAME"],
-			'password' => $_SERVER["DB1_USER"],
-			'charset'  => $_SERVER["DB1_PASS"],
+			'username' => $_SERVER["DB1_USER"] ,
+			'password' => $_SERVER["DB1_PASS"],
+			'charset'  => 'utf8',
 			'prefix'   => '',
 		),
 
